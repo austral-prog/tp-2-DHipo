db4f2f45207bef0b9e77f2326d94ffd820c47787
DHipo <104331457+DHipo@users.noreply.github.com>
2024-03-25 08:55:42 -0300

    Ejercicio 3

--- change.py
+++ change.py
@@ -1,3 +1,13 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:",expense)
+    print("Dinero recibido:", money)
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(int(money-expense))
+    print("Centavos:")
+    print((money - expense) - int(money-expense))
