792b34b16c38255b83c105bf6f20e96c1196a8c9
DHipo <104331457+DHipo@users.noreply.github.com>
2024-03-26 09:26:19 -0300

    update files

--- change.py
+++ change.py
@@ -2,12 +2,14 @@ def change():
     expense = 23.75
     money = 100
 
-    print("Ingresar gasto:",expense)
-    print("Dinero recibido:", money)
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
     print()
     print("Vuelto")
     print()
     print("Pesos:")
     print(int(money-expense))
     print("Centavos:")
-    print((money - expense) - int(money-expense))
+    print(int(((money - expense) - int(money-expense)) * 100))
