bee1914a28ae91a891342d0d778a48b1288c6541
DHipo <104331457+DHipo@users.noreply.github.com>
2024-03-26 09:17:43 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    result = (x < y)
+    print(f"The result of { x if x < y else y} comes first in the dictionary than { x if x > y else y} is {result}.")
+    print(f"The result of { x if x >  y else y} comes first in the dictionary than { x if x < y else y} is {not result}.")
